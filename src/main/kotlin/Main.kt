1cb156f80e992b99a0a27612705d3b9bb0d9d215
geehhyy <devkhun409@gmail.com>
2023-07-11 22:45:43 +0900

    연산자 > 증감연산자

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,23 +2,24 @@ import chapter01.CarManufacturer
 import chapter01.Student
 
 fun main(args: Array<String>) {
-    var a1 = 10
-    var a2 = -a1
-    println("a2 : ${a2}")
+    var a5 = 10
+    var a6 = 10
+    var a7 = a5++ // a5 = a5 + 1
+    var a8 = a6-- // a6 = a6 - 1
+    println("a5, a7 : ${a5}, ${a7}")
+    println("a6, a8 : ${a6}, ${a8}")
 
-    a2 = a1.unaryMinus()
-    println("a2 : ${a2}")
+    a5 = 10
+    a6 = 10
+    a7 = ++a5
+    a8 = --a6
+    println("a5, a7 : ${a5}, ${a7}")
+    println("a6, a8 : ${a6}, ${a8}")
 
-    a2 = +a1
-    println("a2 : ${a2}")
-
-    a2 = a1.unaryPlus()
-    println("a2 : ${a2}")
-
-    var a3 = true
-    var a4 = !a3
-    println("a4 : ${a4}")
-
-    a4 = a3.not()
-    println("a4 : ${a4}")
+    a5 = 10
+    a6 = 10
+    a7 = a5.inc()
+    a8 = a6.dec()
+    println("a5, a7 : ${a5}, ${a7}")
+    println("a6, a8 : ${a6}, ${a8}")
 }
