00b1451cb9469257de71726cb132e2fccfe3a882
geehhyy <devkhun409@gmail.com>
2023-07-11 22:47:03 +0900

    연산자 > 산술연산자

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,34 @@ import chapter01.Student
 
 fun main(args: Array<String>) {
     var a5 = 10
-    var a6 = 10
-    var a7 = a5++ // a5 = a5 + 1
-    var a8 = a6-- // a6 = a6 - 1
-    println("a5, a7 : ${a5}, ${a7}")
-    println("a6, a8 : ${a6}, ${a8}")
+    var a6 = 3
+    var a7 = a5 + a6
+    var a8 = a5.plus(a6)
+    println("a7 : ${a7}")
+    println("a8 : ${a8}")
 
-    a5 = 10
-    a6 = 10
-    a7 = ++a5
-    a8 = --a6
-    println("a5, a7 : ${a5}, ${a7}")
-    println("a6, a8 : ${a6}, ${a8}")
+    a7 = a5 - a6
+    a8 = a5.minus(a6)
+    println("a7 : ${a7}")
+    println("a8 : ${a8}")
 
-    a5 = 10
-    a6 = 10
-    a7 = a5.inc()
-    a8 = a6.dec()
-    println("a5, a7 : ${a5}, ${a7}")
-    println("a6, a8 : ${a6}, ${a8}")
+    a7 = a5 * a6
+    a8 = a5.times(a6)
+    println("a7 : ${a7}")
+    println("a8 : ${a8}")
+
+    a7 = a5 / a6
+    a8 = a5.div(a6)
+    println("a7 : ${a7}")
+    println("a8 : ${a8}")
+
+    a7 = a5 % a6
+    a8 = a5.rem(a6)
+    println("a7 : ${a7}")
+    println("a8 : ${a8}")
+
+    var a10 = a6..a5
+    var a11 = a6.rangeTo(a5)
+    println("a10 : ${a10}")
+    println("a11 : ${a11}")
 }
