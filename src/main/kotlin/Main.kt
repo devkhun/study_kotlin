057aa2c325cab20c93aabe09fc4f1a03a3ec1a78
geehhyy <devkhun409@gmail.com>
2023-07-18 00:27:33 +0900

    연산자 > 비교연산자

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,35 +2,16 @@ import chapter01.CarManufacturer
 import chapter01.Student
 
 fun main(args: Array<String>) {
-    var a5 = 10
-    var a6 = 3
-    var a7 = a5 + a6
-    var a8 = a5.plus(a6)
-    println("a7 : ${a7}")
-    println("a8 : ${a8}")
+    var a13 = 10
+    var a14 = a13 == 10
+    println("a14 : ${a14}")
 
-    a7 = a5 - a6
-    a8 = a5.minus(a6)
-    println("a7 : ${a7}")
-    println("a8 : ${a8}")
+    a14 = a13.equals(10)
+    println("a14 : ${a14}")
 
-    a7 = a5 * a6
-    a8 = a5.times(a6)
-    println("a7 : ${a7}")
-    println("a8 : ${a8}")
+    a14 = a13 != 10
+    println("a14 : ${a14}")
 
-    a7 = a5 / a6
-    a8 = a5.div(a6)
-    println("a7 : ${a7}")
-    println("a8 : ${a8}")
-
-    a7 = a5 % a6
-    a8 = a5.rem(a6)
-    println("a7 : ${a7}")
-    println("a8 : ${a8}")
-
-    var a10 = a6..a5
-    var a11 = a6.rangeTo(a5)
-    println("a10 : ${a10}")
-    println("a11 : ${a11}")
+    a14 = !a13.equals(10)
+    println("a14 : ${a14}")
 }
